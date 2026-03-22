f7634e5b83249a999cff8001c876f223fb0bdf5b
aww-sam <guragaisamyog30@gmail.com>
2026-03-23 02:06:41 +0545

    added app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,jsonify,render_template
+import joblib
+import pandas as pd
+app=Flask(__name__)
+# Loading Models
+model = joblib.load('models/disease_pred.pkl')
+le = joblib.load('models/label_encoder.pkl')
+feature_columns = joblib.load('models/feature_cols.pkl')
+
+def parse_symptoms(text):
+    text=text.lower()
+    symptoms={}
+    for col in feature_columns:
+        readable_text=col.replace("_"," ")
+        symptoms[col]=(1 if readable_text in text or col in text else 0)
+    return symptoms
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index.html', symptoms=[col.replace("_"," ") for col in feature_columns])
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=request.get_json()
+
+    #checking if input is correct
+    if not data or 'symptoms' not in data:
+        return jsonify({'Error':'Please provide symptoms'})
+    symptoms=parse_symptoms(data['symptoms'])
+    detected=[k.replace('_',' ') for k,v in symptoms.items() if v==1]
+    if not detected:
+        return jsonify({"Error":"Can't recognize symptoms" })
+    
+    ## predict that data and inversetransform it
+    input_df=pd.DataFrame([symptoms])
+    prediction=model.predict(input_df)
+    disease_recognized=le.inverse_transform(prediction)[0]
+
+    return jsonify({
+        'predicted_disease':disease_recognized,
+        'symptoms_detected':detected
+    })
+
+@app.route('/symptoms',methods=['GET'])
+def get_symptoms():
+    return jsonify({
+        'symptoms':[col.replace("_"," ") for col in feature_columns]
+        })
+if __name__=="__main__":
+    app.run(debug=True)
